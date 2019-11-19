589fa1664c441b00934781ede987bb166342da83
MouraYuri <mourayuri339@gmail.com>
2019-11-19 18:41:55 -0300

    done

--- dwnn.py
+++ dwnn.py
@@ -7,7 +7,7 @@ def preProcessingData(data):
         data[x] = data[x].split(' ')
         data[x][1] = data[x][1].rstrip()
         data[x][0], data[x][1] = int(data[x][0]), int(data[x][1])
-    df = pd.DataFrame(data, columns = ['xCordinate', 'yCordinate'])
+    df = pd.DataFrame(data, columns = ['xCoordinate', 'yCoordinate'])
     return df
 
 def weight(x, xi, sigma):
@@ -16,10 +16,10 @@ def weight(x, xi, sigma):
 def dwnn(dataset, x, sigma):
     u, d = 0, 0
     for z in range(len(dataset)):
-        xi, yi = dataset.loc[z, 'xCordinate'], dataset.loc[z, 'yCordinate']
+        xi, yi = dataset.loc[z, 'xCoordinate'], dataset.loc[z, 'yCoordinate']
         u, d = u + (yi*weight(xi, x, sigma)), d + weight(xi, x, sigma)
     y = u/d
-    plt.scatter(dataset.loc[:, 'xCordinate'], dataset.loc[:,'yCordinate'], color='blue')
+    plt.scatter(dataset.loc[:, 'xCoordinate'], dataset.loc[:,'yCoordinate'], color='blue')
     plt.scatter(x, y, color='red')
     plt.show()
 dataset = open('./DWNN/dataset.txt', 'r')
